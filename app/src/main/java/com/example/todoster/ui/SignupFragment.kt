e437702c71ef0b1cdba45b0e5f1d1327b8457e89
Nikita Tihonov <jagarnikita@gmail.com>
2025-07-11 18:46:57 +0200

    add go back action

--- app/src/main/java/com/example/todoster/ui/SignupFragment.kt
+++ app/src/main/java/com/example/todoster/ui/SignupFragment.kt
@@ -1,7 +1,18 @@
 package com.example.todoster.ui
 
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.todoster.R
 
-class SignupFragment() : Fragment(R.layout.sign_up_fragment)
+class SignupFragment() : Fragment(R.layout.sign_up_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val signupButton = view.findViewById<View>(R.id.btn_signup)
+        signupButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+}
